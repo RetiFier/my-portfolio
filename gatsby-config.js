06ffa95a34a9cc02b34a79e93dd1090cb4e31b9d
Reti Fier <50056219+RetiFier@users.noreply.github.com>
2022-03-16 16:14:53 +0630

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
